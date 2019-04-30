1d088a0052578664e7d6e7722ee995c9f3a9281a
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-04-30 18:18:37 -0400

    deleted unnecessary line of code

--- instagram-app/src/components/PostContainer/PostsPage.js
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import dummyData from '../../dummy-data';
 import PostContainer from './PostContainer';
 import SearchComponent from '../SearchBar/SearchComponent';
