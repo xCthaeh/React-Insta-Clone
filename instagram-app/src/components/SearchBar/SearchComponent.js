6bc38cfc89c035e31f1dca78296edc289e31d04d
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-05-01 18:38:56 -0400

    Converted search bar css to styled components. MVP for the week complete

--- instagram-app/src/components/SearchBar/SearchComponent.js
+++ instagram-app/src/components/SearchBar/SearchComponent.js
@@ -1,36 +1,93 @@
-import React from 'react';
-import './Search.css';
+/* eslint-disable no-unused-vars */
+/**@jsx jsx */
+import React from "react";
+import { jsx, css } from "@emotion/core";
 
-class SearchComponent extends React.Component{
-  constructor(props){
-    super(props)
+const search_component_container = css`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+  padding-bottom: 30px;
+  border-bottom: 0.5px solid #d3d3d3;
+`;
+
+const search_component_instagram_logo = css`
+  display: flex;
+  justify-content: flex-start;
+`;
+const instagram_icon = css`
+  margin-right: 25px;
+  height: 36px;
+  width: 36px;
+  margin-right: 20px;
+`;
+
+const vertical_line = css`
+  border-left: 2.5px solid black;
+  height: 35px;
+`;
+const ig_logo = css`
+  height: 40px;
+  margin-left: 20px;
+`;
+
+const search_input = css`
+  color: black;
+  font-size: 1.1rem;
+  text-align: center;
+  height: 15px;
+  width: 250px;
+  padding: 10px;
+  background-color: #fafafa;
+  background-image: url("./search.png");
+  background-position: 7px center;
+  background-repeat: no-repeat;
+`;
+const icons = css`
+  height: 34px;
+  width: 34px;
+  margin-right: 20px;
+`;
+
+class SearchComponent extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      searchInput: '',
-    }
+      searchInput: ""
+    };
   }
 
   render() {
     return (
-      <div className="search-component-container">
-        <div className="search-component-instagram-logo">
-          <img className="icons instagram_icon" alt="instagram icon" src="./../img/instagram-logo.png" />
-          <div className="vertical_line"></div>
-          <img className="instagram_logo" alt="instagram logo" src="./../img/instagram.png" />
+      <div css={search_component_container}>
+        <div css={search_component_instagram_logo}>
+          <img
+            css={instagram_icon}
+            alt="instagram icon"
+            src="./../img/instagram-logo.png"
+          />
+          <div css={vertical_line} />
+          <img
+            css={ig_logo}
+            alt="instagram logo"
+            src="./../img/instagram.png"
+          />
         </div>
         <div className="input">
-        <input className="search-input"
-          type="text"
-          placeholder="Search"
-          onChange={this.props.searchPostHandler}/>
+          <input
+            css={search_input}
+            type="text"
+            placeholder="Search"
+            onChange={this.props.searchPostHandler}
+          />
         </div>
         <div className="search-component-navigation">
-          <img className="icons" alt="browse" src="./../img/compass.png" />
-          <img className="icons" alt="likes" src="./../img/heart.png" />
-          <img className="icons" alt="profile" src="./../img/profile.png" />
+          <img css={icons} alt="browse" src="./../img/compass.png" />
+          <img css={icons} alt="likes" src="./../img/heart.png" />
+          <img css={icons} alt="profile" src="./../img/profile.png" />
         </div>
-
       </div>
-    )
+    );
   }
 }
 
